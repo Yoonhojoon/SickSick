0c09111f72a235c08d36229be8b7124b8f042cbb
dbsghwns <dbsghwns1209@gmail.com>
2025-01-28 20:47:10 +0900

    Move Spring Boot project to backend directory

---
+++ backend/src/main/java/com/example/sicksick/SicksickApplication.java
@@ -0,0 +1,13 @@
+package com.example.sicksick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicksickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicksickApplication.class, args);
+	}
+
+}
